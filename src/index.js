de2e211ecb260d46a0721dfc8096cec5bf8554fa
sabbir <sabbirsarkar27@gmail.com>
2023-07-27 20:07:09 +0600

    component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function Greeting() {
+    return (
+        <div>
+            <Person/>
+
+            <h1>hey</h1>
+            <Message/>
+            
+        </div>
+    );
+}
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+    return <p>this is my message</p>;
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting/>);
