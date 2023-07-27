5e54be2b1dba8b4bcb92c70643ac40612fd35ce3
sabbir <sabbirsarkar27@gmail.com>
2023-07-27 21:14:26 +0600

    css added to booklist

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 const BookList=() => {
-    return <section>
-        <Book/>
-    </section>
+    return (<section className='booklist'>
+        <Book />
+        <Book />
+        <Book />
+
+    </section>);
 }
 const Book = () => {
-    return <article>
+    return <article className='book'>
         <Image />
         <Title />
         <Author/>
     </article>
 }
-const Image = () => <h2>image Placeholder</h2>
-const Title = () => <h2>Book Title</h2>
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81yfsIOijJL._AC_UL600_SR600,400_.jpg" alt='American Prometheus: The Triumph and Tragedy'/>
+const Title = () => <h2>American Prometheus: The Triumph and Tragedy</h2>
 const Author = () => {
-    return <h2>Author</h2>;
+    return <h2> Martin J. Sherwin </h2>;
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Book/>);
+root.render(<BookList/>);
