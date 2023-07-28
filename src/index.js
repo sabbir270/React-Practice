fdab8cc1220b2668656bf614599b892eeb585086
sabbir <sabbirsarkar27@gmail.com>
2023-07-28 09:45:27 +0600

    version 3 on props

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ const BookList=() => {
 
     </section>);
 }
-const Book = (props) => {
-    const {img,title,author}=props
+const Book = ({img,title,author}) => {
+    
     return <article className='book'>
         <img src={img} alt={title} />
         <h2>{ title }</h2>
