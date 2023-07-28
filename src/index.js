c58c77a7442c3c6035b4496ab65015b6d2fcc8f2
sabbir <sabbirsarkar27@gmail.com>
2023-07-28 10:38:02 +0600

    children props

--- src/index.js
+++ src/index.js
@@ -16,18 +16,22 @@ const secondBook = {
 
 const BookList=() => {
     return (<section className='booklist'>
-        <Book title={ firstBook.title } author={firstBook.author} img={firstBook.img} />
+        <Book title={firstBook.title} author={firstBook.author} img={firstBook.img} >
+        <p>Consequat eu occaecat reprehenderit Lorem tempor quis aute aliqua ex pariatur fugiat. Id aliqua excepteur ex reprehenderit incididunt dolore eiusmod voluptate ipsum ex et consectetur culpa cillum. Pariatur sunt voluptate qui ea tempor in nostrud quis ullamco nostrud nulla proident cillum est. Est dolor non mollit pariatur eu eiusmod exercitation commodo in mollit amet nisi nostrud do. Do duis quis do velit deserunt eiusmod.</p>
+        <button>click me</button>
+        </Book>
         <Book title={ secondBook.title } author={secondBook.author} img={secondBook.img} />
         
 
     </section>);
 }
-const Book = ({img,title,author}) => {
+const Book = ({img,title,author,children}) => {
     
     return <article className='book'>
         <img src={img} alt={title} />
         <h2>{ title }</h2>
         <h4>{author}</h4>
+        {children}
 
     </article>
 }
