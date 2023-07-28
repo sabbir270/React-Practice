730f40d2ff7b2d65a62f4ea5b5ab8d9d9a506b8c
sabbir <sabbirsarkar27@gmail.com>
2023-07-28 17:40:46 +0600

    list

--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
-    author: 'Martin J. Sherwin',
-    title: 'American Prometheus: The Triumph and Tragedy',
-    img: './images/book-1.jpg',
-}
-
-const secondBook = {
-    author: 'Hanya Yanagihara',
-    title: 'A Little Life Paperback',
-    img: './images/book-2.jpg',
-}
-
+const books = [
+    {
+        author: 'Martin J. Sherwin',
+        title: 'American Prometheus: The Triumph and Tragedy',
+        img: './images/book-1.jpg',
+        id:1,
+    },
+    {
+        author: 'Hanya Yanagihara',
+        title: 'A Little Life Paperback',
+        img: './images/book-2.jpg',
+        id:2,
+    },
+]
 const BookList=() => {
     return (<section className='booklist'>
-        <Book title={firstBook.title} author={firstBook.author} img={firstBook.img} >
-        <p>Consequat eu occaecat reprehenderit Lorem tempor quis aute aliqua ex pariatur fugiat. Id aliqua excepteur ex reprehenderit incididunt dolore eiusmod voluptate ipsum ex et consectetur culpa cillum. Pariatur sunt voluptate qui ea tempor in nostrud quis ullamco nostrud nulla proident cillum est. Est dolor non mollit pariatur eu eiusmod exercitation commodo in mollit amet nisi nostrud do. Do duis quis do velit deserunt eiusmod.</p>
-        <button>click me</button>
-        </Book>
-        <Book title={ secondBook.title } author={secondBook.author} img={secondBook.img} />
+        {books.map((book) => {
+            const {img,title,author,id}=book
+            return <Book img={img} title={title} author={author} key={id}/>
+        })}
         
 
     </section>);
 }
-const Book = ({img,title,author,children}) => {
+const Book = ({img,title,author}) => {
     
     return <article className='book'>
         <img src={img} alt={title} />
         <h2>{ title }</h2>
         <h4>{author}</h4>
-        {children}
 
     </article>
 }
