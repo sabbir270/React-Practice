4d10e39335e9b3529e5e9be3e817b50701c0a1f6
sabbir <sabbirsarkar27@gmail.com>
2023-07-28 09:05:48 +0600

    props added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+const title = 'American Prometheus: The Triumph and Tragedy';
+const author = 'Martin J. Sherwin';
+const img = './images/book-1.jpg';
 const BookList=() => {
     return (<section className='booklist'>
         <Book />
-        <Book />
-        <Book />
+        <Book job="dveloper" />
+        <Book title="random" number={ 22 } />
 
     </section>);
 }
-const Book = () => {
-    const title = 'Martin J. Sherwin';
+const Book = (props) => {
+    console.log(props)
     return <article className='book'>
-        <img src="./images/book-1.jpg" alt='American Prometheus: The Triumph and Tragedy' />
-        <h2>American Prometheus: The Triumph and Tragedy</h2>
-        <h4>{ title }</h4>
+        <img src={img} alt={title} />
+        <h2>{ title }</h2>
+        <h4>{author}</h4>
+        <p>{ props.job}</p>
+        <p>{props.title}</p>
+        <p>{ props.number}</p>
     </article>
 }
 
